cda25a8f26d135e11310c8c1b6d6ce1d78c79c41
Robby Lewis <Robbysim37@gmail.com>
2023-03-03 16:59:41 -0500

    commit

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,5 +1,14 @@
 exports.up = function (knex) {
   // DO YOUR MAGIC
+  return knex.schema.createTable(`cars`, tbl => {
+    tbl.increments()
+    tbl.text("vin").notNullable().unique()
+    tbl.text("make").notNullable()
+    tbl.text("model").notNullable()
+    tbl.text("mileage").notNullable()
+    tbl.text("title")
+    tbl.text("tansmission")
+  })
 };
 
 exports.down = function (knex) {
