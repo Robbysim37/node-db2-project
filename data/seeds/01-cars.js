05c2f64a3fee4da401079006d091553a79fb182e
Robby Lewis <Robbysim37@gmail.com>
2023-03-05 17:46:33 -0500

    wip

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,30 @@
 // STRETCH
+
+const cars = [
+{
+    vin: "111111111",
+    make: "toyota",
+    model: "prius",
+    mileage: 10000,
+    title: "clean",
+    transmission: "manual"
+},
+{
+    vin: "222222222",
+    make: "toyota",
+    model: "corolla",
+    mileage: 23510,
+    title: "salvage"
+},
+{
+    vin: "333333333",
+    make: "ford",
+    model: "focus",
+    mileage: 100,
+}
+]
+
+exports.seed = async function(knex) {
+    await knex("cars").truncate()
+    await knex("cars").insert(cars)
+}
