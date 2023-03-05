9001f9e0932ba494830d8e68d308e91ff6a085a8
Robby Lewis <Robbysim37@gmail.com>
2023-03-05 17:38:20 -0500

    wip

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,10 +1,15 @@
 // DO YOUR MAGIC
 const express = require("express")
-
+const car = require("./cars-model")
 const router = express.Router()
 
 router.get("/", async (req,res,next) => {
-    res.json("getting all cars")
+     try{
+        const cars = await car.getAll()
+        res.json(cars)
+     } catch (err) {
+        next(err)
+     }
 })
 router.get("/:id", async (req,res,next) => {
     res.json(`get car with id ${req.params.id}`)
